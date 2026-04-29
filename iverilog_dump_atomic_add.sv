db3fe0ac7787789b8a69798d28ec344b98103d5b
piyushptiwari1 <piyushptiwari1@users.noreply.github.com>
2026-04-29 14:56:21 +0530

    chore(git): ignore iverilog_dump_*.sv dumpfile modules

--- iverilog_dump_atomic_add.sv
+++
@@ -1,6 +0,0 @@
-module iverilog_dump_atomic_add();
-initial begin
-    $dumpfile("atomic_add.vcd");
-    $dumpvars(0, gpu);
-end
-endmodule
--- iverilog_dump_matadd.sv
+++
@@ -1,6 +0,0 @@
-module iverilog_dump_matadd();
-initial begin
-    $dumpfile("matadd.vcd");
-    $dumpvars(0, gpu);
-end
-endmodule
